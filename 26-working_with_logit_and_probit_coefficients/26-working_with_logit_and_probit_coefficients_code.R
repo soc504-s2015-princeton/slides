e7d9f6b2acca55cc97c436910d1cd756a5957b1c
Angela Dixon <angelad@princeton.edu>
2015-04-05 00:57:45 -0400

    Update slides for polytomous data

---
+++ 26-working_with_logit_and_probit_coefficients/26-working_with_logit_and_probit_coefficients_code.R
@@ -0,0 +1,121 @@
+library(dplyr)
+library(broom)
+library(ggplot2)
+
+load("data/Hanmer.RData")
+df <- tbl_df(x)
+df <- filter(df, !is.na(presvote))
+
+#### Odds and log-odds #####
+
+### proportion
+df %>%
+  filter(white == 1) %>%
+  summarise(prob.white = mean(presvote))
+
+df %>%
+  filter(white == 0) %>%
+  summarise(prob.nonwhite = mean(presvote))
+
+### Odds
+odds.w <- df %>%
+  filter(white == 1) %>%
+  summarise(odds = (mean(presvote) / (1 - mean(presvote))))
+odds.w             
+
+odds.nw <- df %>%
+  filter(white == 0) %>%
+  summarise(odds = (mean(presvote) / (1 - mean(presvote))))
+odds.nw
+
+
+
+### Log - Odds
+log.odds.w <- df %>%
+  filter(white == 1) %>%
+  summarise(logodds = log((mean(presvote) / (1 - mean(presvote)))))
+
+log.odds.nw <- df %>%
+  filter(white == 0) %>%
+  summarise(logodds =  log((mean(presvote) / (1 - mean(presvote)))))
+
+
+### back to probability
+exp(log.odds.w$logodds) / (1 + exp(log.odds.w$logodds))
+exp(log.odds.nw$logodds) / (1 + exp(log.odds.nw$logodds))
+
+#### function
+inv.logit <- function(logitvals) {
+  pred.odds <- exp(logitvals)
+  pred.prob <- pred.odds / (1 + pred.odds)
+  return(pred.prob)
+}
+
+inv.logit(log.odds.w$logodds)
+inv.logit(log.odds.nw$logodds)
+
+
+####### One varible logisitics regression
+
+fit1 <- glm(presvote ~  white, data=df, family = binomial)
+fit1 <- tidy(fit1)
+
+#### calculate odds 
+fit1$estimate
+fit1$estimate[1] + fit1$estimate[2] ## 0.3807725
+log.odds.w$logodds ## 0.3807725
+
+## odds ratio
+exp(fit1$estimate[2])
+
+## odds.w$odds /odds.nw$odds  ## odds ratio
+
+### predicted probabilities 
+
+(exp(fit1$estimate[1] + fit1$estimate[2])) / (1 + exp(fit1$estimate[1] + fit1$estimate[2]))
+
+exp(fit1$estimate[1]) / (1 + exp(fit1$estimate[1]))
+
+##### replication from Hanmer and Kalkan
+fit2 <- glm(presvote ~ retecon + partyid + bushiraq + ideol7b + white + female +
+             age + educ1_7 + income, data=df, family = binomial)
+tidy(fit2)
+
+
+## observed value approach
+df.white <- df
+df.nonwhite <- df
+
+df.white$white <- 1
+df.nonwhite$white <- 0 
+
+pp.white <- predict(fit2, type = "response", newdata = df.white)
+mean(pp.white)
+
+pp.nonwhite <- predict(fit2, type = "response", newdata = df.nonwhite)
+mean(pp.nonwhite)
+
+###  marginal effect - observed value
+mean(pp.white) - mean(pp.nonwhite) ###  0.007900108
+
+
+## average case approach
+df.mean <- with(df, data.frame(ideol7b = 4, retecon = 0,
+                               white = c(0, 1), female = 1, age = mean(age), 
+                               educ1_7 = 5, income = 16, partyid = 3,
+                               bushiraq = (1/3)))
+df.mean
+df.mean$pp <- predict(fit2, newdata = df.mean, type = "response")
+df.mean
+
+##  marginal effect - average case
+df.mean$pp[2] - df.mean$pp[1] ## 0.03552846
+
+### create data frame for graph
+graph <- df.mean
+graph$pp.white.ova <- c(mean(pp.nonwhite), mean(pp.white))
+graph 
+
+ggplot(graph, aes(x = as.factor(white), y = pp)) + geom_point(color = "red") +
+  geom_point(aes(y = pp.white.ova), color = "blue") 
+
---
+++ 29-working_with_polytomous_data/29-working_with_polytomous_data_code.R
@@ -0,0 +1,84 @@
+
+##### Load data
+#install.packages("nnet")
+library(nnet)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(stargazer)
+
+load("data/Hanmer.RData")
+df <- tbl_df(x)
+df <- filter(df, !is.na(presvote))
+
+
+## create 3 level factor of party identification
+df2 <- df %>%
+mutate(party = ifelse(df$partyid == 0 | df$partyid == 1, "dem",  ## Democrats
+               ifelse(df$partyid == 2 | df$partyid == 3 | df$partyid == 4, "indep",  ## Independents
+               ifelse(df$partyid == 5 | df$partyid == 6, "rep", NA))))     ## Republicans 
+                            
+df2
+
+######## multinomial 
+fit3 <- multinom(party ~ presvote + retecon  + bushiraq + ideol7b + white + female +
+              age + educ1_7 + income, data=df2)
+summary(fit3)
+stargazer(fit3, type = "text")
+
+
+### observed value approach
+df2.w <- df2
+df2.nw <- df2
+
+df2.w$white <- 1
+df2.nw$white <- 0 
+
+df2.w <- cbind(predict(fit3, type = "probs", newdata = df2.w), df2.w)
+tbl_df(df2.w)
+mean(df2.w$dem)
+mean(df2.w$indep)
+mean(df2.w$rep)
+
+
+df2.nw <- cbind(predict(fit3, type = "probs", newdata = df2.nw), df2.nw)
+tbl_df(df2.nw)
+mean(df2.nw$dem)
+mean(df2.nw$indep)
+mean(df2.nw$rep)
+
+
+
+### average case approach
+df2.mean <- with(df2, data.frame(ideol7b = 4, retecon = 0,
+                                 white = c(0, 1), female = 1, age = mean(age), 
+                                 educ1_7 = 5, income = 16,
+                                 bushiraq = (1/3),
+                                 presvote = 1))
+
+
+df2.mean
+df3 <-cbind(predict(fit3, newdata = df2.mean, type = "probs"), df2.mean)
+df3
+
+## data frame for graph
+df3 <- gather(df3, key, value, dem:rep) 
+
+## observed value
+ova.value <- c(mean(df2.nw$dem), mean(df2.w$dem), mean(df2.nw$indep), mean(df2.w$indep),
+               mean(df2.nw$rep), mean(df2.w$rep))
+
+df3$ova.value <- ova.value
+df3
+
+## graph for whites
+ggplot(filter(df3, white == 1), aes(x = key, y = value )) + geom_point(aes(color = as.factor(white))) +
+  geom_point(color = "red") +
+  geom_point( aes(y = ova.value), color = "blue") +
+  ggtitle("predicted probabilities, whites")
+
+## graph for nonwhites
+ggplot(filter(df3, white == 0), aes(x = key, y = value )) +
+  geom_point(color = "red") +
+  geom_point( aes(y = ova.value), color = "blue") +
+  ggtitle("predicted probabilities, nonwhites")
