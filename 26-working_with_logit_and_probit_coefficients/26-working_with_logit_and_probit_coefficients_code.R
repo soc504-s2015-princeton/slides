6d770a014d223ba6e802c6a92282027c9253330c
Angela Dixon <angelad@princeton.edu>
2015-04-06 13:44:33 -0400

    fix typos

--- 26-working_with_logit_and_probit_coefficients/26-working_with_logit_and_probit_coefficients_code.R
+++ 26-working_with_logit_and_probit_coefficients/26-working_with_logit_and_probit_coefficients_code.R
@@ -34,25 +34,26 @@ odds.nw
 log.odds.w <- df %>%
   filter(white == 1) %>%
   summarise(logodds = log((mean(presvote) / (1 - mean(presvote)))))
+log.odds.w
 
 log.odds.nw <- df %>%
   filter(white == 0) %>%
   summarise(logodds =  log((mean(presvote) / (1 - mean(presvote)))))
-
+log.odds.nw
 
 ### back to probability
 exp(log.odds.w$logodds) / (1 + exp(log.odds.w$logodds))
 exp(log.odds.nw$logodds) / (1 + exp(log.odds.nw$logodds))
 
 #### function
-inv.logit <- function(logitvals) {
+Inv.logit <- function(logitvals) {
   pred.odds <- exp(logitvals)
   pred.prob <- pred.odds / (1 + pred.odds)
   return(pred.prob)
 }
 
-inv.logit(log.odds.w$logodds)
-inv.logit(log.odds.nw$logodds)
+Inv.logit(log.odds.w$logodds)
+Inv.logit(log.odds.nw$logodds)
 
 
 ####### One varible logisitics regression
